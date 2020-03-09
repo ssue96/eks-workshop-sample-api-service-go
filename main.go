e95442ca6eb8d731791e0d7ce9de0a0e3d29b08b
ssue96 <jhsjsd@naver.com>
2020-03-09 18:04:34 +0900

    trigger test

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hola3 World"}
+		res := &response{Message: "hello World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
